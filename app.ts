a46b4ca5b02f85901506e3e8d8824b7ec10b8989
Dex <sheepareawesome777@gmail.com>
2022-11-29 21:11:00 +0300

    ESNext module resolution

--- app.ts
+++ app.ts
@@ -1,3 +1,15 @@
 "use strict";
 
-//
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+|       Solves Sudoku puzzles (duh)        |
+| https://github.com/Lamby777/sudoku-solve |
+\*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// dumbass requirement for top-level await
+export {}
+
+const puzzle = await readPuzzle();
+
+async function readPuzzle() {
+	//fetch("./file.json").then(res => json = res.json())
+}
